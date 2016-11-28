3934d1a80f6c92254cc0741fc2bcdaae3284f90a
Adam Gajzlerowicz <adam.gajzlerowicz@performancehorizon.com>
2016-11-28 16:58:52 +0100

    Started adding in redux

--- dev/index.js
+++ dev/index.js
@@ -15,7 +15,6 @@ const onChange = (val) => {
 
 render((
     <div>
-        <Select items={items} onChange={onChange} tabIndex="1"/>
         <Select items={items} onChange={onChange} tabIndex="2"/>
     </div>
 ), document.getElementById('app'));
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -29,9 +29,9 @@ module.exports = {
             {
                 test: /\.jsx?$/,         // Match both .js and .jsx files
                 exclude: /node_modules/,
-                loader: 'babel-loader',
+                loader: 'babel',
                 query: {
-                    presets: ['react', 'es2015']
+                    presets: ['react', 'es2015', 'stage-0', 'airbnb']
                 }
             },
 
